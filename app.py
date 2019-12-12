1a1e0ea5f212943ec946ba58bbc0d582c585ae72
Sage Rimal <srimal@campusexplorer.com>
2019-12-12 13:02:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ def create_show_submission():
     db.session.close()
     return render_template('pages/home.html')
  
-
+'''on error'''
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
